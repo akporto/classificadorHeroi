ed613db94f8b776e589ef43fd7ac962bdfaabd1a
Ana Kellen Nogueira <86116339+akporto@users.noreply.github.com>
2024-10-14 22:49:55 -0400

    primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let nomeHeroi = "Thor";
+let experiencia = 7500;
+
+
+let nivel;
+
+
+if (experiencia < 1000) {
+  nivel = "Ferro";
+} else if (experiencia <= 2000) {
+  nivel = "Bronze";
+} else if (experiencia <= 5000) {
+  nivel = "Prata";
+} else if (experiencia <= 7000) {
+  nivel = "Ouro";
+} else if (experiencia <= 8000) {
+  nivel = "Platina";
+} else if (experiencia <= 9000) {
+  nivel = "Ascendente";
+} else if (experiencia <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
